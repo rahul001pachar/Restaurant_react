00db34e3022bbf824edf9920534ec60704ffcd1a
Rahul Pachar <rahulpachar0786@gmail.com>
2023-07-17 11:51:19 +0530

    added menu cart

--- src/Components/Basics/Restaurant.js
+++ src/Components/Basics/Restaurant.js
@@ -1,26 +1,31 @@
-import React from 'react'
+import React,{useState} from "react";
 import "./style.css";
-const Restaurant = () => {
-  return (
+import Menu from "./menuApi.js";
+import MenuCard from "./MenuCard";
+
+const Restaurant = () =>{
+    const [menuData,setMenuData] = useState(Menu);
+
+    const filterItem=(category) =>{
+        const updatedList= Menu.filter((curElem)=>{
+            return curElem.category===category;
+        });
+        setMenuData(updatedList);
+    };
+    return (
     <>
-        <div className="card-container">
-            <div className="card">
-                <div className="card-body">
-                    <span className="card-number card-circle subtle">1</span>
-                    <span className="card-author subtle" style={{color:"cyan"}}>Breakfast</span>
-                    <h2 className="card-title">Maggi</h2>
-                    <span classname="card-description subtle">
-                        HI this is maggi.Price rs 10. 
-                    </span>
-                    <div className="card-read">Read</div>
-                </div>
-                <img src={image} alt="images" className="card-media"/>
-                 
-                 <span className="card-tag subtle">Order Now</span>
-            </div>
+    <nav className="navbar">
+        <div className="btn-group">
+            <button className="btn-group__item" onClick={()=>filterItem("breakfast")}>Breakfast</button>
+            <button className="btn-group__item" onClick={()=>filterItem("lunch")}>Lunch</button>
+            <button className="btn-group__item" onClick={()=>filterItem("evening")}>Evening</button>
+            <button className="btn-group__item" onClick={()=>filterItem("dinner")}>Dinner</button>
+            <button className="btn-group__item" onClick={()=>setMenuData(Menu)}>All</button>
         </div>
+    </nav>
+    <MenuCard menuData={menuData}/>
     </>
-  );
+    );
 };
 
-export default Restaurant
+export default Restaurant;
